c56f1d216197f88d73deaaa28fc07a7b85a573a9
root <root@opensourcefoundation.(none)>
2013-11-21 19:19:02 +0000

    Fixed stupid server balls

--- GeoServer/views.py
+++ GeoServer/views.py
@@ -23,29 +23,6 @@ def check(request):
 
 @csrf_exempt
 def receive(request):
-    """The methods for the registration of a multiple user"""
     if request.method == 'POST':
-
-        encoded = request.POST['data']
-
-        decoded = encoded
-        try:
-            decoded = json.loads(encoded)
-        except ValueError:
-            return HttpResponse('Not json')
-
-        finalList = []
-
-        for user in decoded:
-            a = User.create(
-                name = user['name'],
-                password = user['password'],
-                mobile_number = user['mobile_number'],
-                user_type = user['user_type']
-            )
-            a.save()
-            finalList.append((a.userid, a.name))
-
-        return HttpResponse(str(finalList))
-
-    return HttpResponse('404, man')
+        data = request.POST
+        print data
