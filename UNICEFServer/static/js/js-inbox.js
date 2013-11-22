fbe25ffef96fa129a95657b4974cf4af25328b63
Alexander James Wallar <aw204@st-andrews.ac.uk>
2013-11-22 01:03:49 +0000

    Added stactic

--- UNICEFServer/static/js/js-inbox.js
+++ UNICEFServer/static/js/js-inbox.js
@@ -1,8 +1,68 @@
+var map;
+
+function getInbox(){
+	$(document).load('sample.json',function(data){ renderInbox(data); });
+}
+
+function renderInbox(data){
+	
+	// Parse to JSON
+	JSONdata = $.parseJSON(data);
+	
+	// Create our map object
+	
+	
+	$.each(JSONdata,function(key,value){
+		// For each inbox item
+		// Populate a property array
+		message[];
+		$.each(value,function(key,value){
+			message[key] = value;
+			});
+		// Now populate
+		});
+}
+
+function initialize() {
+        var mapOptions = {
+          center: new google.maps.LatLng(4.8380, 31.5842),
+          zoom: 12
+        };
+		map = new google.maps.Map(document.getElementById("mapoutput"),
+            mapOptions);
+		
+		putonmarkers();
+      }
+	  
+	  function putonmarkers(){
+	  
+	  var myLatlng = new google.maps.LatLng(4.8380,31.5842);
+var marker = new google.maps.Marker({
+position: myLatlng,
+map: map,
+icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=1|FF0000|000000',
+title:"Hello World!"
+});
+
+	var myLatlng2 = new google.maps.LatLng(10,1);
+var marker2 = new google.maps.Marker({
+position: myLatlng2,
+map: map,
+icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=2|FF9900|000000',
+title:"Hello World2!"
+});
+
+	  }
+
+
+// Process the click event on the messages
 $(document).ready(function(){
-	var map = L.map('mapoutput');
-	L.tileLayer('http://{s}.tile.cloudmade.com/42205C4BED464DCEABC785E965100D77/997/256/{z}/{x}/{y}.png', {
-			maxZoom: 18
-		}).addTo(map);
-	var mapCentre = L.latLng(7.885,26.949)
-	map.setView(mapCentre).setZoom(6);
+	setTimeout('getInbox()',1000);
+	$('#leftsidebar .item').click(function(){
+		if($(this).attr('lat')!=null){
+			newCenter = new google.maps.LatLng($(this).attr('lat'),$(this).attr('long'));
+			map.setCenter(newCenter);
+			map.setZoom(14);
+		}
+	});
 });
